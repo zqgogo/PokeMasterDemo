369a450f97fdb645b2c34c7df995200ad3be1d13
zhangqian <>
2023-10-18 16:24:20 +0800

    1.初始化

---
+++ PokeMasterDemo/PokeMasterDemo/PokeMasterDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeMasterDemoApp.swift
+//  PokeMasterDemo
+//
+//  Created by qilitech.ltd on 2023/10/18.
+//
+
+import SwiftUI
+
+@main
+struct PokeMasterDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
