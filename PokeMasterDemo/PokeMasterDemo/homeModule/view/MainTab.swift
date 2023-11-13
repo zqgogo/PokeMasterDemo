921d4f64c6641fea81fa94eec4a11f271bb39d13
zhangqian <>
2023-11-13 10:29:16 +0800

    tabbar的各项属性设置

--- PokeMasterDemo/PokeMasterDemo/homeModule/view/MainTab.swift
+++ PokeMasterDemo/PokeMasterDemo/homeModule/view/MainTab.swift
@@ -8,13 +8,32 @@
 import SwiftUI
 
 struct MainTab: View {
+    
+    init() {
+        // 目前设置tabbar 各项item，只能如此设置。
+        let appear = UITabBarItemAppearance()
+        appear.normal.iconColor = .brown
+        appear.normal.titleTextAttributes = [.foregroundColor: UIColor(.brown)]
+        
+        appear.selected.iconColor = .orange
+        appear.selected.titleTextAttributes = [.foregroundColor: UIColor(.red)]
+        
+        let tabbarAppear = UITabBarAppearance()
+        tabbarAppear.stackedLayoutAppearance = appear
+        
+        UITabBar.appearance().standardAppearance = tabbarAppear
+        UITabBar.appearance().backgroundColor = .white
+        UITabBar.appearance().backgroundImage = UIImage()
+        //        UITabBar.appearance().unselectedItemTintColor = .red
+    }
+    
     var body: some View {
         TabView {
             // 1
             PokemonRootView().tabItem {
                 // 2
-                Image(systemName: "list.bullet.below.rectangle")
-                Text("列表")
+                Image(systemName: "list.bullet.below.rectangle").foregroundColor(.black)
+                Text("列表").foregroundColor(.black)
             }
             SettingRootView().tabItem {
                 Image(systemName: "gear")
